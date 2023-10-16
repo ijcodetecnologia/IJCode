e2fecb5e340170ef5ae60f1c36fb36ef14b5ec73
IJCode <ijcode.oficial@gmail.com>
2023-10-15 21:32:56 -0300

    Update MenuMobile.js

--- src/components/UI/MenuMobile.js
+++ src/components/UI/MenuMobile.js
@@ -1,10 +1,21 @@
-'use client';
-import { Link } from 'react-scroll';
-import React, { useState } from 'react';
-import styles from './MenuMobile.module.css';
+"use client";
+import { Link } from "react-scroll";
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./MenuMobile.module.css";
 
 const MenuMobile = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMobileMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -17,47 +28,52 @@ const MenuMobile = () => {
         className={`${styles.listMobile}  ${
           mobileMenu ? styles.listMobileActive : styles.listMobile
         }`}
+        ref={menuRef}
       >
         <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              href=""
-            >
-              Home
-            </Link>
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          href=""
+          onClick={() => setMobileMenu(!mobileMenu)}
+        >
+          Home
+        </Link>
         <Link
-              to="vantagens"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              href=""
-            >
-              Vantagens
-            </Link>
+          to="vantagens"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          href=""
+          onClick={() => setMobileMenu(!mobileMenu)}
+        >
+          Vantagens
+        </Link>
         <Link
-              to="beneficios"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              href=""
-            >
-              Beneficíos
-            </Link>
+          to="beneficios"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          href=""
+          onClick={() => setMobileMenu(!mobileMenu)}
+        >
+          Beneficíos
+        </Link>
         <Link
-              to="orcamento"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              href=""
-            >
-              Orçamento
-            </Link>
+          to="orcamento"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          href=""
+          onClick={() => setMobileMenu(!mobileMenu)}
+        >
+          Orçamento
+        </Link>
       </nav>
     </div>
   );
